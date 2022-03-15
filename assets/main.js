1c6453dbcd9f8c1762189ef0167379dc91b6d183
Bipin7giri <98257522+Bipin7giri@users.noreply.github.com>
2022-03-15 23:50:11 +0545

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+// sepcial countdown
+let dayItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+let countdown = () => {
+  let futureDate = new Date("30 MARCH 2022");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+  let sec = Math.floor(myDate / 1000) % 60;
+  dayItem.innerHTML = days;
+  hoursItem.innerHTML = hours;
+  minItem.innerHTML = min;
+  secItem.innerHTML = sec;
+};
+countdown();
+setInterval(countdown, 1000);
